09b1ebdde0e77dd19f01a4c9e8f2f417554075a7
Denir <denir-junior19@live.com>
2021-05-09 12:21:49 -0300

    feat(master):initial implementation

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'app/dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './app/src/app.html',
+            filename: 'app.html',
+        })
+    ]
+};
