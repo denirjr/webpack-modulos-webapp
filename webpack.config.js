e6a2fb260446aad7c0ea3a578bd768e3419a14eb
Denir <denir-junior19@live.com>
2021-05-19 20:15:27 -0300

    feature(master): add css style to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,16 @@ module.exports = {
         path: path.resolve(__dirname, 'app/dist'),
         clean: true,
     },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: './app/src/app.html',
             filename: 'app.html',
             hash: true,
-        })
+        }),
     ]
 };
